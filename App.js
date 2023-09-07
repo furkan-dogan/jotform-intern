70ebd0fd7a15102ac7116d3efbe36614dbbe7565
Furkan <dofurkan@gmail.com>
2023-09-07 14:48:30 +0300

    created pages and interfaces

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import StartScreen from './src/screens/StartScreen';
+import ResetPasswordScreen from './src/screens/ResetPasswordScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
-const App = () => {
+const Stack = createStackNavigator();
+
+export default function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World !</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="StartScreen"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="StartScreen" component={StartScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen
+          name="ResetPasswordScreen"
+          component={ResetPasswordScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-export default App;
+}
