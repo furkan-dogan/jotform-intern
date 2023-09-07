509aa1e4cc81e16055a82b49edf73157310c46b7
Furkan <dofurkan@gmail.com>
2023-09-07 10:35:34 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World !</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
